7709fc5777400bddebb0c17a854588329c64a883
ENEmy <nosde@protonmail.com>
2022-03-15 18:32:46 +0700

    refactor function name for main search route

--- backend/routes/search.py
+++ backend/routes/search.py
@@ -11,5 +11,5 @@ def export_routes(route:str, router:FastAPI, db:Session):
         tags=[route[1:], 'no_tokens_required'],
         response_model=search_schema.SearchItems
     )
-    def search(keyword: str, offset:int = 0, limit:int = 100):
+    def search_for_users_and_pages(keyword: str, offset:int = 0, limit:int = 100):
         return controller.search_for_users_and_pages(db, keyword, offset, limit)
